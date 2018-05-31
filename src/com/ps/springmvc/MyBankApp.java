d74d546bb0599accbad972c4629c517c257e0ca7
Quang Hoa <quanghoahcm@gmail.com>
2018-05-31 14:24:21 +0700

    Inversion of control

--- src/com/ps/springmvc/MyBankApp.java
+++ src/com/ps/springmvc/MyBankApp.java
@@ -1,14 +1,21 @@
 package com.ps.springmvc;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.ps.springmvc.model.CurrentAccount;
 import com.ps.springmvc.model.SavingAccount;
 
 public class MyBankApp {
 
+	private static ClassPathXmlApplicationContext context;
+
 	public static void main(String []args) {
-		SavingAccount svaccount = new SavingAccount();
+	/*	SavingAccount svaccount = new SavingAccount();
 		CurrentAccount craccount = new CurrentAccount();
-		System.out.println(svaccount.createAccount());
-		System.out.println(craccount.createAccount());
+		System.out.println(svaccount.createAccount());*/
+		
+		context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Account account = context.getBean("myAccount",Account.class);		
+		System.out.println(account.createAccount());
 	}
 }
