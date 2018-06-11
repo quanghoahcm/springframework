eda660c21ce70d75dff965988cd9c1700526a474
Quang Hoa <quanghoahcm@gmail.com>
2018-06-11 10:15:34 +0700

    jhipster import-jdl ./my-jdl-file.jdl --force

---
+++ jhipster-demo/src/main/java/com/jhipster/service/mapper/RegionMapper.java
@@ -0,0 +1,24 @@
+package com.jhipster.service.mapper;
+
+import com.jhipster.domain.*;
+import com.jhipster.service.dto.RegionDTO;
+
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity Region and its DTO RegionDTO.
+ */
+@Mapper(componentModel = "spring", uses = {})
+public interface RegionMapper extends EntityMapper<RegionDTO, Region> {
+
+
+
+    default Region fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Region region = new Region();
+        region.setId(id);
+        return region;
+    }
+}
